8e0d04e54a2ba3643eb0a0ddfdf728a9d1cb23e6
Vishal K <testingC@gmail.com>
2025-09-16 17:34:06 -0400

    Flutter project created

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            color: Colors.black,
+            width:300,
+            height:300,
+          )
+        )
+      ),
+    );
+  }
+}
